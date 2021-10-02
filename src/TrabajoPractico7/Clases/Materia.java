947e73e506b794d7915cdd4ac140190b2a954dea
Agostina-Fernandez <fernandez_agostina96@hotmail.com>
2021-10-02 01:36:18 -0300

    Fernández Agostina - Trabajo Práctico 6 - Laboratorio de Programación I - ULP

--- src/TrabajoPractico7/Clases/Materia.java
+++ src/TrabajoPractico7/Clases/Materia.java
@@ -5,4 +5,43 @@ public class Materia {
     private String nombre;
     private int anio;
     
+    
+    public Materia(int idMateria, String nombre, int anio){
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+    
+    /* GETTERS */
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    /* SETTERS */
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
 }
